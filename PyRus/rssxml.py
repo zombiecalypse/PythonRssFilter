7086ee063d2e964782e3426d215ce0abd000b2a0
Aaron Karper <akarper@students.unibe.ch>
2012-06-13 16:42:25 +0200

    New output engine
    no direct xml generation for extra robustness

--- PyRus/rssxml.py
+++ PyRus/rssxml.py
@@ -1,46 +1,49 @@
-from lxml.objectify import ElementMaker
-from lxml.etree import tostring
+from PyRSS2Gen import *
+from datetime import datetime
+from time import mktime
 
-E= ElementMaker(annotate=False)
 
 def xml_string(rss):
-    return tostring(rss_xml(rss), pretty_print =True)
+    return rss_xml(rss).to_xml(encoding='utf8')
 
 def rss_xml(rss):
     "RssFeed -> Xml"
     feed = rss['feed']
-    return E.rss(
-            E.channel(
-                E.title(feed['title']),
-                *map(xml_item, rss['entries'])
-                ),
-            version = "2.0"
-            )
+    return RSS2(
+            title = feed['title'],
+            description = "Merged Feed '{}'".format(feed['title']),
+            link = "http://google.com",
+            lastBuildDate = datetime.utcnow(),
+            items = map(xml_item, rss['entries']))
 
 def try_to_make_guid(item):
     try:
-        return E.guid(item['id'], isPermaLink = str(item['guidislink']).lower())
+        return Guid(item['id'])
     except KeyError as e:
-        return E.guid(item['link'], isPermaLink = 'true')
+        return Guid(item['link'])
 
 def try_to_make_pubdate(item):
 	try:
-		return E.pubDate(item['updated'])
+		return datetime.fromtimestamp(mktime(item['updated_parsed']))
 	except KeyError as e:
 		return None
 
 def try_to_make_description(item):
     try:
-        return E.description(item['description'])
+        item['description'] = sanitize(item['description'])
+        return item['description']
     except Exception as e:
         print e
-        print item['description']
+        print repr(item['description'])
+
+def sanitize(string):
+    return unicode(string)
 
 def xml_item(item):
     "Item -> Xml"
-    return E.item(
-            E.title(item['title']),
-            try_to_make_pubdate(item),
-            try_to_make_description(item),
-            try_to_make_guid(item),
-            E.link(item['link']))
+    return RSSItem(
+            title = item['title'],
+            link = item['link'],
+            pubDate = try_to_make_pubdate(item),
+            description = try_to_make_description(item),
+            guid = try_to_make_guid(item))
